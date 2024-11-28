36994a2db921d38fb8368509b66599a73b0ca482
VI Nguyen <nguyenhungvicb@gmail.com>
2024-11-28 16:35:38 +0700

    update

--- minesweeper/server.js
+++ minesweeper/server.js
@@ -1,22 +1,24 @@
 const http = require("node:http");
 
-const server = http.createServer((req, res) => {
-  res.end();
-});
+const server = http.createServer();
 
 server.on("clientError", (err, socket) => {
   socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
+});
+
+server.on("request", (req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      hello: "world",
+    }),
+  );
 });
 
 const PORT = 8000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-server.on("request", (req) => {
-  console.log(req.headers);
-});
-
 /*
  * - A socket can either be alive or destroyed depended on the "keepAlive" option
    - The "keepAlive" option is not the same as "Connection: keep-alive" header
