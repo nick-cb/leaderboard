c3e9d2c41f1fe6316dfe9914785d8940b3045365
VI Nguyen <nguyenhungvicb@gmail.com>
2024-12-10 20:43:49 +0700

    update

--- watcher/test/background-task.js
+++ watcher/test/background-task.js
@@ -1,25 +1,55 @@
 const { describe, it } = require("node:test");
+const assert = require("node:assert");
 const { watch } = require("../index.js");
 const { spawn } = require("child_process");
 
 describe("watcher background task test", () => {
   it("should be able to kill subprocess's child processes", (_, done) => {
     const childProcess = watch([null, null, "test/background-script.js"]);
-    console.log(childProcess.pid)
+    const childProcessPid = childProcess.pid;
     let subprocessPid;
+    childProcess.on("spawn", () => {
+      const ps = spawn("ps", ["-A", "-o", "ppid,pid"]);
+      /** @type {string} data */
+      let data;
+      ps.stdout.on("data", (chunk) => {
+        if (!data) {
+          data = chunk;
+          return;
+        }
+        data+=chunk;
+      });
+      ps.stdout.on("end", () => {
+        assert.equal(typeof data === "string", true);
+        assert.equal(typeof childProcessPid === "number", true);
+        console.log(data.includes(childProcessPid), childProcessPid)
+        assert.equal(data.includes(childProcessPid), true);
+      })
+    });
+
     childProcess.on("message", (msg) => {
       subprocessPid = msg;
     });
+
     setTimeout(() => {
-      console.log("kill child process");
       childProcess.kill();
       const ps = spawn("ps", ["-A", "-o", "ppid,pid"]);
-      ps.stdout.on("data", (msg) => {
-        if (Buffer.isBuffer(msg) && subprocessPid.pid) {
-          console.log({subprocessPid})
-          console.log("include " + subprocessPid.pid + ": " + msg.toString().includes(subprocessPid.pid));
+      /** @type {string} data */
+      let data;
+      ps.stdout.on("data", (chunk) => {
+        if (!data) {
+          data = chunk;
+          return;
         }
+        data+=chunk;
       });
-    }, 1000);
+      ps.stdout.on("end", () => {
+        assert.equal(typeof data === "string", true);
+        assert.equal(typeof childProcessPid === "number", true);
+        console.log(data.includes(childProcessPid), childProcessPid)
+        assert.equal(data.includes(childProcessPid), false);
+        done();
+      });
+    }, 3000);
   });
 });
