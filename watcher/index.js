2ed85cd635c19b6a1c13a96a6a35df48a29535c8
VI Nguyen <nguyenhungvicb@gmail.com>
2024-12-10 15:15:44 +0700

    update

---
+++ watcher/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const { fork } = require("child_process");
+
+function watch(args) {
+  let script = args[2];
+  if (!script && fs.existsSync("./index.js")) {
+    script = "index.js";
+  }
+  const cwd = process.cwd();
+
+  let childProcess = fork(script);
+  fs.watch(cwd, { recursive: true }, (event, filename) => {
+    if (filename.includes("node_modules")) {
+      return;
+    }
+    childProcess.kill();
+    childProcess = fork(script);
+  });
+}
+
+module.exports = { watch };
