ba593cf00d3d2a552748cde46b20170fb6466ed2
VI Nguyen <nguyenhungvicb@gmail.com>
2024-12-11 16:07:46 +0700

    update

--- watcher/test/background-script.js
+++ watcher/test/background-script.js
@@ -1,19 +1,25 @@
-const { fork, spawn } = require("child_process");
+const { spawn } = require("child_process");
 
-if (process.send) {
-  process.send({ pid: process.pid });
-}
+// if (process.send) {
+//   process.send({ pid: process.pid });
+// }
 
-spawn(
-  'sh',
+const childProcess = spawn(
+  "sh",
   [
-    '-c',
+    "-c",
     `node -e "
       console.log(process.pid, 'is alive')
       setTimeout(() => {
         console.log(process.pid, 'stop')
       }, 20000);
-    "`
+    "`,
   ],
-  {stdio: ['inherit', 'inherit', 'inherit']}
+  // { stdio: ["inherit", "inherit", "inherit"] },
 );
+
+childProcess.stdout.on("data", (chunk) => {
+  const data = chunk.toString();
+  const pid = data.split(" ");
+  process.send({ subPid: parseInt(pid[0]) });
+});
--- watcher/test/background-task.js
+++ watcher/test/background-task.js
@@ -1,4 +1,5 @@
 const { describe, it } = require("node:test");
+const util = require("node:util");
 const os = require("os");
 const assert = require("node:assert");
 const { watch } = require("../index.js");
@@ -7,93 +8,54 @@ const { spawn } = require("child_process");
 describe("watcher background task test", () => {
   it("shouldn't be able to kill child processes's child processes", (_, done) => {
     const childProcess = watch([null, null, "test/background-script.js"]);
-    const childProcessPid = childProcess.pid;
-    let subprocessPid;
-    childProcess.on("spawn", () => {
-      // const ps = spawn("ps", ["-A", "-o", "ppid,pid"]);
-      // /** @type {string} data */
-      // let data;
-      // ps.stdout.on("data", (chunk) => {
-      //   if (!data) {
-      //     data = chunk;
-      //     return;
-      //   }
-      //   data += chunk;
-      // });
-      // ps.stdout.on("end", () => {
-      //   data = data.toString();
-      //   assert.equal(typeof data === "string", true);
-      //   assert.equal(typeof childProcessPid === "number", true);
-      //   assert.equal(data.includes(childProcessPid), true);
-      // });
+    childProcess.on("spawn", async () => {
+      const pidList = await getPidList();
+      assert.equal(typeof pidList === "string", true);
+      assert.equal(typeof childProcess.pid === "number", true);
+      assert.equal(pidList.includes(childProcess.pid), true);
     });
 
+    let childProcessSubProcessPid;
     childProcess.on("message", (msg) => {
-      console.log(childProcessPid, msg)
-      // subprocessPid = msg.pid;
-      // const ps = spawn("ps", ["-A", "-o", "ppid,pid"]);
-      // /** @type {string} data */
-      // let data;
-      // ps.stdout.on("data", (chunk) => {
-      //   if (!data) {
-      //     data = chunk;
-      //     return;
-      //   }
-      //   data += chunk;
-      // });
-      // ps.stdout.on("end", () => {
-      //   data = data.toString();
-      //   console.log(
-      //     data.split("\n").filter((line) => line.includes(subprocessPid)),
-      //   );
-      // });
+      console.log({ msg });
+      if (typeof msg === "object") {
+        childProcessSubProcessPid = msg.subPid;
+      }
     });
 
     setTimeout(() => {
-      console.log("killed child process")
+      console.log("killed child process");
       childProcess.kill();
-      // childProcess.on("exit", () => {
-      //   const ps = spawn("ps", ["-A", "-o", "ppid,pid"]);
-      //   let data;
-      //   ps.stdout.on("data", (chunk) => {
-      //     if (!data) {
-      //       data = chunk;
-      //       return;
-      //     }
-      //     data += chunk;
-      //   });
-      //   ps.stdout.on("end", () => {
-      //     data = data.toString();
-      //     // console.log(
-      //     //   data.split("\n").filter((line) => line.includes(subprocessPid)),
-      //     // );
-      //   });
-      // });
-      // const ps = spawn("ps", ["-A", "-o", "ppid,pid"]);
-      // /** @type {string} data */
-      // let data;
-      // ps.stdout.on("data", (chunk) => {
-      //   if (!data) {
-      //     data = chunk;
-      //     return;
-      //   }
-      //   data += chunk;
-      // });
-      // ps.stdout.on("end", () => {
-      //   data = data.toString();
-      //   assert.equal(typeof data === "string", true);
-      //   assert.equal(typeof childProcessPid === "number", true);
-      //   assert.equal(data.includes(childProcessPid), false);
-      //   if (os.platform() !== "linux") {
-      //     // console.log(
-      //     //   data.split("\n").filter((line) => line.includes(subprocessPid)),
-      //     // );
-      //     assert.equal(data.includes(subprocessPid), false);
-      //   } else {
-      //     assert.equal(data.includes(subprocessPid), true);
-      //   }
-      //   done();
-      // });
+      childProcess.on("exit", async () => {
+        const pidList = await getPidList();
+        assert.equal(typeof pidList === "string", true);
+        assert.equal(typeof childProcess.pid === "number", true);
+        assert.equal(pidList.includes(childProcess.pid), false);
+        assert.equal(pidList.includes(childProcessSubProcessPid), true);
+        done();
+      });
     }, 3000);
   });
 });
+
+async function getPidList() {
+  const ps = spawn("ps", ["-A", "-o", "ppid,pid"]);
+  /** @type {Promise<string>} promise */
+  const promise = await new Promise((resolve) => {
+    /** @type {string} data */
+    let data;
+    ps.stdout.on("data", (chunk) => {
+      if (!data) {
+        data = chunk;
+        return;
+      }
+      data += chunk;
+    });
+    ps.stdout.on("end", () => {
+      data = data.toString();
+      resolve(data);
+    });
+  });
+
+  return promise;
+}
